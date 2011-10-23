2b680c4ec85da1446bf552a9bed254675c087641
Tobias Vogt <tobi@webguys.de>
2011-10-23 13:02:08 +0200

    local-stuff

--- src/app/code/community/Fod/Cutesave/test/saveproduct.php
+++ src/app/code/community/Fod/Cutesave/test/saveproduct.php
@@ -1,19 +1,20 @@
 <?php
 
-require dirname(__FILE__).'/../../../../../Mage.php';
+// require dirname(__FILE__).'/../../../../../Mage.php';
+require '/home/tobias/www/foocamp/app/Mage.php';
 Mage::app('admin');
 
 for($i=0; $i<= 50; $i++) {
 
-$product = Mage::getModel('catalog/product');
+$product = Mage::getModel('catalog/product')->load( 6 );
 $product->setStoreId(0);
 $product->setWebsiteId(1);
 $product->setData('sku', 'z' . $i);
 $product->setData('name', 'name y' . $i);
 $product->setData('description', 'Description ' . $i);
 $product->setData('short_description', 'Description ' . $i);
-$product->setCategoryIds(array(10, 22));
-$product->setAttributeSetId('9');
+//$product->setCategoryIds(array(10, 22));
+$product->setAttributeSetId('4');
 $product->setTypeId('simple');
 $product->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH);
 $product->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED);
@@ -22,15 +23,41 @@ $product->setTaxClassId(0);
 
 $product->setWeight(0);
 
+   
+$sizes = array('S','M','XL');
+$options = array(
+        'is_require' => true,
+        'sort_order' => '1',
+        'title' => 'Größe',
+        'type' => 'drop_down',
+        'values' => array()
+);
+foreach( $sizes AS $size ) {
+    $options['values'][] = array(
+        'price' => 0,
+        'price_type' => 'fixed', // 'percent'
+        'sku' => '',
+        'sort_order' => '0',
+        'title' => $size,
+     );
+}
+$product->setProductOptions( array( 6 => $options) );
+$product->setHasOptions(1);
+$product->setCanSaveCustomOptions(1);
 
-$product->setStockData(
-    array(
-        'is_in_stock' => 1,
-         'qty' => 1
-));
 
-// $product->save();
 
+//$product->setStockData(
+//    array(
+//        'is_in_stock' => 1,
+//         'qty' => 1111
+//));
+
+$product->save(); echo $product->getId();
+
+
+
+exit;
 
 Mage::getSingleton('fod_cutesave/queue')->add( $product );
 
